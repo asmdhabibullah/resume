acaf6cec82bc12290142049c96f7d6af007fffc8
asmdhabibullah <asmdhabibullah@yahoo.com>
2022-01-06 07:05:34 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ServicesComponent } from './components/services/services.component';
+import { RecommendationsComponent } from './components/recommendations/recommendations.component';
+import { ProtfolioComponent } from './components/protfolio/protfolio.component';
+import { WorksComponent } from './components/works/works.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ServicesComponent,
+    RecommendationsComponent,
+    ProtfolioComponent,
+    WorksComponent,
+    FooterComponent,
+    ContactComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
